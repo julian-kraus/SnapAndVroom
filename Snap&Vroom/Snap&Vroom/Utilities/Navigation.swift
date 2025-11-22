94b9bec9f602342830d20428d7793316d55f3617
Neli Shahapuni <nelishahapuni@gmail.com>
2025-11-22 12:01:20 +0100

    remove

--- Snap&Vroom/Snap&Vroom/Utilities/Navigation.swift
+++ Snap&Vroom/Snap&Vroom/Utilities/Navigation.swift
@@ -7,17 +7,10 @@
 
 import Foundation
 import SwiftUI
-import Combine
 
 class Navigation: ObservableObject {
-    var objectWillChange: ObservableObjectPublisher
-    
     
     @Published var navPath = NavigationPath()
-    
-    init() {
-        objectWillChange = .init();
-    }
     
     enum View: Hashable {
         case pickup
